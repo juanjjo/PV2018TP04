3ff2fac76936692c190f4a4322349a85507c0f34
Juan José Alancay <pc@DESKTOP-KL2HFPD>
2018-05-05 17:41:24 -0300

    punto 1 resuelto Agregue pagina temperatura, y paquete aplicacion.punto1.beans.forms

---
+++ src/java/aplicacion/punto1/beans/forms/OpcionTemperaturaFormBean.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.punto1.beans.forms;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author pc
+ */
+@ManagedBean
+@ViewScoped
+public class OpcionTemperaturaFormBean {
+private String eleccion;
+    private double ingreso;
+    private double equivalencia;
+    public OpcionTemperaturaFormBean() {
+    }
+
+    public void tranformar (){
+        if(eleccion.equals("FAR")){
+            equivalencia= 1.8*ingreso;
+        }else{
+            if(eleccion.equals("CEL")){
+                equivalencia= (ingreso-32)/1.8;
+            }
+        }
+    }
+    public String getEleccion() {
+        return eleccion;
+    }
+
+    /**
+     * @param eleccion the eleccion to set
+     */
+    public void setEleccion(String eleccion) {
+        this.eleccion = eleccion;
+    }
+
+    /**
+     * @return the ingreso
+     */
+    public double getIngreso() {
+        return ingreso;
+    }
+
+    /**
+     * @param ingreso the ingreso to set
+     */
+    public void setIngreso(double ingreso) {
+        this.ingreso = ingreso;
+    }
+
+    /**
+     * @return the equivalencia
+     */
+    public double getEquivalencia() {
+        return equivalencia;
+    }
+
+    /**
+     * @param equivalencia the equivalencia to set
+     */
+    public void setEquivalencia(double equivalencia) {
+        this.equivalencia = equivalencia;
+    }
+    
+}
